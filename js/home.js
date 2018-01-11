8e7945d5a838d2369fc0534264712c23d815d79a
shouston3 <shouston3@me.com>
2018-01-11 01:21:17 +0000

    update elm files and add static reload image

--- js/home.js
+++ js/home.js
@@ -1,2 +1,19 @@
-var home_node = document.querySelector('#home');
-var home_app = Elm.Home.embed(home_node);
+var homeNode = document.querySelector('#home');
+var homeApp = Elm.Home.embed(homeNode);
+
+var socket = new Phoenix.Socket('/socket', {});
+
+socket.connect();
+
+homeApp.ports.sync.subscribe(function (o) {
+  var channel = socket.channel('sync:deck', {});
+
+  channel.on('sync_msg', function (payload) {
+    homeApp.ports.syncMessage.send(payload.body);
+  });
+
+  channel.join()
+    .receive('ok', function () {})
+    .receive('error', console.error);
+});
+
